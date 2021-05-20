c1eaf067e3a914e94a52a438119ee4c585b7d140
MedLam95 <lhamouz.mehdi@gmail.com>
2021-05-20 12:33:48 +0200

    Connexion base de données

--- pages/newArticle.php
+++ pages/newArticle.php
@@ -2,6 +2,19 @@
 
 require __DIR__ . '/partials/startTheme.php';
 
+if (!empty($_POST)) {
+    // ETAPE 1 : Se connécter à la base de données
+    $pdo = new PDO('mysql:dbname=php-poo-blog;host=localhost', 'root');
+    var_dump('Nous sommes connécté à la base de données');
+
+    // ETAPE 2 : Envoyer une requête de création d'article
+    $title = $_POST['title'];
+    $description = $_POST['description'];
+    $content = $_POST['content'];
+
+    var_dump($title, $description, $content);
+}
+
 ?>
 <h1>Page de création d'article</h1>
 
