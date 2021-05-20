8210b07df022b5ff017b29f9ccc4b4afd60c16ff
MedLam95 <lhamouz.mehdi@gmail.com>
2021-05-20 10:18:09 +0200

    Ajout condition notfound

--- src/App.php
+++ src/App.php
@@ -10,22 +10,33 @@ class App
      * Cette méthode demarre notre application. C'est ici
      * que l'on trouve le code initiale.
      */
-    static public function start(): void
+    static public function start()
     {
         // $_GET permet d'accèder au query string
         // ex: $_GET['page'] retourne la query string "page"
         // la fonction isset(...) permet de tester si un
         // élement est présent dans un tableaux.
 
-        $page = 'list';
+        $pageName = 'list';
 
         // Ici on test si on a envoyé la query "page"
         if (isset($_GET['page'])) {
-            $page = $_GET['page'];
+            // Si la query page existe dans ce cas nous l'assignons
+            // à notre variable page
+            $pageName = $_GET['page'];
         }
 
-        // ici afficher la page avec "require" !
+        // file_exists('/chemin/vers/le/fichier.php') retourne true
+        // si le fichier existe, false sinon.
 
-        require __DIR__ . '/../pages/' . $page . '.php';
+        // ETAPE 2 : Nous affichons la page demandée
+
+        $pagePath = __DIR__ . '/../pages/' . $pageName . '.php';
+
+        if (file_exists($pagePath)) {
+            require $pagePath;
+        } else {
+            require __DIR__ . '/../pages/notFound.php';
+        }
     }
 }
